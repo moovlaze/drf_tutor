22ae8d2348e0ffd19c027a19ffa8bfa18a15985b
Moovlaze <max.moovlaze@gmail.com>
2025-01-21 23:43:26 +0300

    work with base class APIView

--- drf_tutor/women/serializers.py
+++ drf_tutor/women/serializers.py
@@ -2,8 +2,29 @@ from rest_framework import serializers
 
 from women.models import Women
 
+class WomenSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=255)
+    content = serializers.CharField()
+    time_create = serializers.DateTimeField(read_only=True)
+    time_updated = serializers.DateTimeField(read_only=True)
+    is_published = serializers.BooleanField(default=True)
+    car_id = serializers.IntegerField()
 
-class WomenSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Women
-        fields = ('title', 'cat_id')
+    def create(self, validated_data):
+        return Women.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.content = validated_data.get('content', instance.content)
+        instance.time_create = validated_data.get('time_create', instance.time_create)
+        instance.time_updated = validated_data.get('time_updated', instance.time_updated)
+        instance.is_published = validated_data.get('is_published', instance.is_published)
+        instance.cat_id = validated_data.get('cat_id', instance.cat_id)
+        instance.save()
+        return instance
+
+
+# class WomenSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Women
+#         fields = ('title', 'cat_id')
--- drf_tutor/women/views.py
+++ drf_tutor/women/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import Women
 from .serializers import WomenSerializer
 
+class WomenAPIView(APIView):
+    def get(self, request):
+        serialize = WomenSerializer(Women.objects.all())
+        print(serialize.data)
+        return Response({'Post': 'this is my post'})
 
-class WomenAPIView(generics.ListAPIView):
-    queryset = Women.objects.all()
-    serializer_class = WomenSerializer
+
+# class WomenAPIView(generics.ListAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
